c9f96bd57edcc914bd5f683760ae0bae793a8b87
Wongwc1998 <wongwc1998@gmail.com>
2023-09-23 16:42:16 -0400

    add filter

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,19 +2,20 @@ import { useState } from 'react'
 
 const NumbersDisplay = ({ numbers }) => {
   return numbers.map(number => {
-    return (<p key={number.name}>{number.name} {number.number}</p>);
+    return (<p key={number.id}>{number.name} {number.number}</p>);
   })
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
   const addName = (event) => {
     event.preventDefault();
     console.log('button clicked', event.target);
@@ -30,7 +31,12 @@ const App = () => {
     <div>
       <div>debug name: {newName}</div>
       <div>debug number: {newNumber}</div>
+      <div>debug filter: {filter}</div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
