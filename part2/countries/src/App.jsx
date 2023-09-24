0d33b5837d8952b4e5e00f79e688d689514ec7db
Wongwc1998 <wongwc1998@gmail.com>
2023-09-24 16:05:51 -0400

    add boilerplate code

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const App = () => {
+
+  return (
+    <div>
+      find countries <input />
+    </div>
+  )
+
+}
+
+export default App
