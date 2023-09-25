0573787a2c56d114d1a48d7f201c543aafea3f1a
Wongwc1998 <wongwc1998@gmail.com>
2023-09-24 20:25:15 -0400

    add weather

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,8 +1,26 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 import countriesService from './services/countries'
 
 const CountriesDisplay = ({ filter, countries, shownArray, setShownArray }) => {
+  const [weather, setWeather] = useState(null);
   const filteredCountries = countries.filter((country) => country.name.common.toLowerCase().includes(filter));
+  useEffect(() => {
+    if (filteredCountries.length === 1) {
+      const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${filteredCountries[0].capital}&appid=${api_key}&units=metric`;
+
+      axios.get(apiURL)
+        .then((response) => {
+          setWeather(response.data);
+        })
+        .catch(error => {
+          console.error("Error fetching weather data: ", error);
+          setWeather(null);
+        });
+    } else {
+      setWeather(null);
+    }
+  }, [filter]);
   if (filteredCountries.length === 1) {
     const country = filteredCountries[0];
     return (<div>
@@ -14,6 +32,14 @@ const CountriesDisplay = ({ filter, countries, shownArray, setShownArray }) => {
         {Object.values(country.languages).map((language) => <li key={language}>{language}</li>)}
       </ul>
       <img src={country.flags.png} alt="flag" width="100" height="100" />
+      {weather && (
+        <div>
+          <h2>Weather in {country.capital[0]}</h2>
+          <p>Temperature {weather.main.temp} Celsius</p>
+          <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} />
+          <p>Wind {weather.wind.speed} m/s</p>
+        </div>)
+      }
     </div >);
   }
   else if (filteredCountries.length < 10) {
@@ -52,6 +78,7 @@ const CountriesDisplay = ({ filter, countries, shownArray, setShownArray }) => {
 }
 
 const App = () => {
+  console.log(api_key);
   const [input, setInput] = useState('')
   const [countries, setCountries] = useState([])
   const [shownArray, setShownArray] = useState(Array(10).fill(false))
